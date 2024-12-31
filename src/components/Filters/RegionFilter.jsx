5fbc57bdcd6aff542ab897b6137892c973cce18c
rubenbot77 <rubenbot77@hotmail.com>
2024-12-31 13:59:21 -0500

    enhance region filter functionality and add search input; improve styling for country container

--- src/components/Filters/RegionFilter.jsx
+++ src/components/Filters/RegionFilter.jsx
@@ -3,20 +3,24 @@ import CountryContext from "../../context/CountryContext"
 export const RegionFilter = () => {
     const { regionFilter, setRegionFilter } = useContext(CountryContext);
     const regions = ['Americas', 'Antarctic', 'Africa', 'Asia', 'Europe', 'Oceania']; 
-    
+    const handleRegionClick = (region) => {
+        if (regionFilter.includes(region)) {
+          setRegionFilter(regionFilter.filter((r) => r !== region));
+        } else {
+          setRegionFilter([...regionFilter, region]);
+        }
+    };
+
+
     return (
         <div className="region-filter">
             <p className="region-title">Region</p>
             <ul className="regions-container">
                 {regions.map((region, index) => {
                     return (
-                        <li key={index} className={`regions-item ${regionFilter.includes(region) ? 'regions-item-active' : ''}`} onClick={() => {
-                            if(regionFilter.includes(region)) {
-                                setRegionFilter(regionFilter.filter(item => item !== region));
-                            } else {
-                                setRegionFilter([...regionFilter, region]);
-                            }
-                        }}>{region}</li>
+                        <li key={index} className={`regions-item ${regionFilter.includes(region) ? 'regions-item-active' : ''}`}
+                        onClick={() => handleRegionClick(region)}
+                        >{region}</li>
                     )
                 })}
             </ul>
--- src/context/CountryContext.jsx
+++ src/context/CountryContext.jsx
@@ -1,6 +1,6 @@
 import  { createContext } from 'react';
 import useFetchCountries from '../customHooks/useFetchContries';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 const CountryContext = createContext();
 
 export const CountryProvider = ({ children }) => {
@@ -10,30 +10,55 @@ export const CountryProvider = ({ children }) => {
   const [ONUMember, setONUMember] = useState(false);
   const [independent, setIndependent] = useState(false);
   const [regionFilter, setRegionFilter] = useState(['Americas', 'Antarctic', 'Africa', 'Asia', 'Europe', 'Oceania']);
+  const [searchInput, setSearchInput] = useState('')
 
-  if(sortFilter === 'population') {
-    countries.sort((a, b) => b.population - a.population)
-  } else if(sortFilter === 'area') {
-    countries.sort((a, b) => b.area - a.area)
-  } else if (sortFilter === 'alphabetical') {
-    countries.sort((a, b) => a.name.common.localeCompare(b.name.common))
-  }
+
   useEffect(() => {
-    setFilteredCountries(countries);
-      if(ONUMember) {
-        setFilteredCountries(countries.filter((country) => country.unMember));
-      }
-      if(independent) {
-        setFilteredCountries(countries.filter((country) => !country.unMember));
-      }
-      if(regionFilter.length > 0) {
-        setFilteredCountries(countries.filter((country) => regionFilter.includes(country.region)));
-      }
-  }, [sortFilter, countries, ONUMember, independent, regionFilter]);
+    let updatedCountries = [...countries.sort((a, b) => b.population - a.population)];
+
+    if (sortFilter === 'population') {
+      updatedCountries.sort((a, b) => b.population - a.population);
+    } else if (sortFilter === 'area') {
+      updatedCountries.sort((a, b) => b.area - a.area);
+    } else if (sortFilter === 'alphabetical') {
+      updatedCountries.sort((a, b) => a.name.common.localeCompare(b.name.common));
+    }
+    if(ONUMember) {
+      updatedCountries = updatedCountries.filter((country) => country.unMember);
+    }
+    if(independent) {
+      updatedCountries = updatedCountries.filter((country) => !country.unMember);
+    }
+    if(regionFilter.length > 0) { 
+      updatedCountries = updatedCountries.filter((country) => regionFilter.includes(country.region));
+    }
+    if(searchInput) {
+      updatedCountries = updatedCountries.filter((country) => country.name.common.toLowerCase().startsWith(searchInput.toLowerCase()));
+    }
+    setFilteredCountries(updatedCountries);
+  }, [sortFilter, countries, ONUMember, independent, regionFilter, searchInput]);
+
+
 
 
   return (
-    <CountryContext.Provider value={{ filteredCountries, countries, loading, error, sortFilter, setSortFilter, setONUMember, setIndependent, ONUMember, independent, regionFilter, setRegionFilter }}>
+    <CountryContext.Provider value={
+      {filteredCountries,
+      countries,
+      loading,
+      error,
+      sortFilter,
+      setSortFilter,
+      setONUMember,
+      setIndependent,
+      ONUMember,
+      independent,
+      regionFilter,
+      setRegionFilter,
+      searchInput,
+      setSearchInput
+      }
+     }>
       {children}
     </CountryContext.Provider>
   );
