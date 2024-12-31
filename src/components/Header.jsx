1adf1c885614a1ae0ff8d76e58ebe18da396d75f
rubenbot77 <rubenbot77@hotmail.com>
2024-12-31 18:48:27 -0500

    update header to display the number of filtered countries using context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import { Search } from "./Search";
+import { useContext } from  "react";
+import CountryContext from "../context/CountryContext";
 
 export const Header = () => {
+    const { filteredCountries } = useContext(CountryContext);
     return (
         <div className="header-container">
-            <h3 className="found-countries-container">Found 234 countries</h3>
+            <h3 className="found-countries-container">Found {filteredCountries.length} countries</h3>
             <Search />
         </div>
     )
